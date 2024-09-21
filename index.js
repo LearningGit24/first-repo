28b29d06c2a82b4c860831c2dd96c6f58dcf357b
LearningGit24 <portgasdkid11@outlook.com>
2024-09-21 23:57:45 +0100

    index.js
    Hello, World!

--- index.js
+++ index.js
@@ -4,3 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Simple Alert</title>
+</head>
+<body>
+    <script>
+        alert("Hello, World!");
+    </script>
+</body>
+</html>
